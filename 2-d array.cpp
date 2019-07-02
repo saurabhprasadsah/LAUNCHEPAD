bcad7bd03fb31b0f8b3e31e22db7c2ee09b3a2bc
saurabh gupta <42804047+saurabhprasadsah@users.noreply.github.com>
2019-07-03 01:08:58 +0530

    c++ files

---
+++ 2-d array.cpp
@@ -0,0 +1,47 @@
+#include<iostrem>
+using namespace std;
+void Findnumber(int a[][100],int key,int row,int col)
+{
+}
+
+int main()
+{
+	int a[100][100];
+	int row,col;
+	cin>>row>>col;
+	  for(int i=0;i<row;i++)
+	  {
+	  	for(int j=0;j<col;j++){
+	  		a[i][j]=i*j;
+		  }
+	  }
+	int key;
+	cout<<"enter key:";
+	cin>>key;
+	cin>>key;
+	
+	
+	
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<col;j++)
+	}
+	if(j=0;j<col;j++){
+		if(a[i][j]==key){
+			break;
+		}
+		
+	}
+	
+	if(j!=col){
+		break;
+	}
+	
+	
+	cout<<"key not found !"<<endl;
+	else
+	cout<<"key  found !"<<i<<","<<j<<
+	
+	
+	return 0;
+}
