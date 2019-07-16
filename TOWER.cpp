e3ae5e6ec6ef4013ef398d3cab211ca46d1885bc
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2019-07-16 20:48:46 +0530

    tower of honoe

---
+++ TOWER.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+void TOH(int n,char source,char helper,char destination){
+	if(n==0){
+		return;
+	}
+	TOH(n-1,source,destination,helper );
+	cout<<"taking disk"<<n<<"from"<<source<<"to"<<destination<<endl;
+	TOH(n-1,helper,source,destination);
+}
+
+
+
+
+int main(){
+	int n;
+	cin>>n;
+	TOH(n,'A','B','C');
+	return 0;
+	
+	
+	
+	
+	
+}
