afcb0019fc558170544ab3826ad4c01afae74516
SAURABH GUPTA <42804047+saurabhprasadsah@users.noreply.github.com>
2019-07-24 21:43:30 +0530

    questionpaper pointer

---
+++ questionpointer.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+   int x=10;
+   int y=100;
+   int *xptr=&
+   
+   
+    cout<<x<<endl;
+    cout<<y<<endl;
+    
+    
+    return 0;
+
+
+
+
+}
+
+
